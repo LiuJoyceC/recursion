e1fce9db3b4afb70b77bef8726bf646881fcee7b
Joyce Liu <liujoycec@gmail.com>
2015-07-29 11:09:59 -0700

    Added an alternate solution in case we're required to have a base case and not allowed to use the _.each function (in the first solution, the base case was not explicitly defined because there was an implied base case due to the nature of _.each doing nothing when the childNodes are empty. The alternate solution also passes the Mocha Spec Runner, but is a less elegant solution than the first.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,7 +7,7 @@
 var getElementsByClassName = function(className, element){
 	// Added a second parameter to allow function to recursively
 	// find child nodes of each element
-	
+
   // your code here
   element = element || document; // First call will have one argument
   								// so element will be set to document
@@ -28,3 +28,35 @@ var getElementsByClassName = function(className, element){
 
   return result;
 };
+
+// alternate solution in case we're required to have a base case and not allowed
+// to use loops (including _.each):
+// but this is a less elegant solution than above
+// Also passes the Mocha Spec Runner (if you want to test it, don't forget to
+// delete the 1 from the function name in both the first and last lines)
+
+var getElementsByClassName1 = function(className, element, remainingNodes) {
+	element = element || document;
+	remainingNodes = remainingNodes || [];
+
+	if (element.classList === undefined || _.indexOf(element.classList, className) === -1) {
+  	var result = [];
+  } else {
+  	var result = [element];
+  }
+
+  // prepending childNodes to remainingNodes (maintains same order as native function)
+  // since childNodes is an array-like object, must convert to true array first using slice
+  remainingNodes = [].slice.apply(element.childNodes).concat(remainingNodes);
+
+  // Base case
+  if (remainingNodes.length === 0) {
+  	return result;
+  }
+
+  // Recursive case
+  else {
+  	return result.concat(getElementsByClassName1(className, remainingNodes.shift(), remainingNodes));
+  }
+  
+}
