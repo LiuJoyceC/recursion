673a34bfb834eb747ed7f23f74e90ac2b4e36927
Joyce Liu <liujoycec@gmail.com>
2015-07-29 13:12:48 -0700

    Created base cases (and error case when a function or undefined is entered), and also created the recursive case for arrays (not yet for objects).

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // Error case
+  if (_.indexOf(['function','undefined'], typeof obj) != -1) {
+    return;
+  }
+
+  // Base case 1
+  else if (typeof obj == 'string') {
+    return '"' + obj + '"';
+  }
+
+  // Base case 2
+  else if (typeof obj != 'object' || obj === null) {
+    return obj + '';
+  }
+
+  // Recursive case
+  else {
+    var result = '';
+    if (Array.isArray(obj)) {
+      _.each(obj,function(element) {
+        result = result + ',' + (stringifyJSON(element) || 'null');
+      });
+      return '[' + result.slice(1) + ']';
+    }
+  }
 };
+/*
+var stringifyJSON = function (obj) {
+  return '9';
+}*/
